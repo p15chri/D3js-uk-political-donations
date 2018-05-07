78d5f621bb34f22e92bf60214f6e5b6d9ac423bc
Chrysa <37374186+p15chri2@users.noreply.github.com>
2018-05-07 22:29:18 +0300

    Update chart.js

--- chart.js
+++ chart.js
@@ -328,6 +328,25 @@ function mouseover(d, i) {
 	var offset = $("svg").offset();
 	
 
+	function mouseover(d, i) {
+	
+	var mosie = d3.select(this);
+	var amount = mosie.attr("amount");
+	var donor = d.donor;
+	var party = d.partyLabel;
+	var entity = d.entityLabel;
+	var offset = $("svg").offset();
+		
+	var mosie = d3.select(this);
+	var amount = mosie.attr("amount");
+	var donor = d.donor;
+	var party = d.partyLabel;
+	var entity = d.entityLabel;
+	var offset = $("svg").offset();
+	var infoBox = "<p> Source: <b>" + donor + "</b></p>"
+								+ "<p> Recipient: <b>" + party + "</b></p>"
+								+ "<p> Type of donor: <b>" + entity + "</b></p>"
+								+ "<p> Total value: <b>&#163;" + comma(amount) + "</b></p>";
 
 	// image url that want to check
 	var imageFile = "https://raw.githubusercontent.com/ioniodi/D3js-uk-political-donations/master/photos/" + donor + ".ico";
